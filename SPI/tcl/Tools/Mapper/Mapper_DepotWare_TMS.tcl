e97ca2a94dce434801c38ddaf80de0b5bd02ab31
Eric Legault-Ouellet <eric.legault-ouellet@ec.gc.ca>
2026-04-10 18:26:36 -0400

    [Mapper] Removed EC TMS as it doesn't exist anymore

--- SPI/tcl/Tools/Mapper/Mapper_DepotWare_TMS.tcl
+++ SPI/tcl/Tools/Mapper/Mapper_DepotWare_TMS.tcl
@@ -18,30 +18,10 @@
 namespace eval Mapper::DepotWare::TMS {
    variable Param
    variable Lbl
-   
+
    set Lbl(XML) { "Description XML" "XML description" }
 
    set Param(Depots) {
-   {       OpenStreetMap_EC TMS { <GDAL_WMS>
-   <Service name="TMS">
-       <ServerUrl>http://osm1.cmc.ec.gc.ca/osm/${z}/${x}/${y}.png</ServerUrl>
-   </Service>
-   <DataWindow>
-      <UpperLeftX>-20037508.34</UpperLeftX>
-      <UpperLeftY>20037508.34</UpperLeftY>
-      <LowerRightX>20037508.34</LowerRightX>
-      <LowerRightY>-20037508.34</LowerRightY>
-      <TileLevel>19</TileLevel>
-      <TileCountX>1</TileCountX>
-      <TileCountY>1</TileCountY>
-      <YOrigin>top</YOrigin>
-   </DataWindow>
-   <Projection>EPSG:900913</Projection>
-   <BlockSizeX>256</BlockSizeX>
-   <BlockSizeY>256</BlockSizeY>
-   <BandsCount>3</BandsCount>
-</GDAL_WMS>} }
-
       { OpenStreetMap TMS { <GDAL_WMS>
    <Service name="TMS">
        <ServerUrl>http://tile.openstreetmap.org/${z}/${x}/${y}.png</ServerUrl>
